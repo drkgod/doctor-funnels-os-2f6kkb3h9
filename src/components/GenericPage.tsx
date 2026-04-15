ee6f18cfeee4822957ab5e8d6ba574ebf74e03e3
Skip <skip@usecurling.com>
2026-04-15 21:58:24 +0000

    Auto-commit before sync

--- src/components/GenericPage.tsx
+++ src/components/GenericPage.tsx
@@ -17,15 +17,4 @@ export function GenericPage({
         !children && 'items-center justify-center min-h-[calc(100vh-10rem)] text-center',
       )}
     >
-      <div className={cn('mb-8', !children && 'mb-0')}>
-        <h1 className="text-3xl lg:text-4xl font-bold tracking-tight text-foreground">{title}</h1>
-        {subtitle && (
-          <p className={cn('text-muted-foreground mt-3 text-lg max-w-xl', !children && 'mx-auto')}>
-            {subtitle}
-          </p>
-        )}
-      </div>
-      {children}
-    </div>
-  )
-}
+      <div className={cn(
